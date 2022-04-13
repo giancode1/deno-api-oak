e1c98f537d28f0b4c56142ec83cd28d4db2809e4
giancode1 <gieg5c@gmail.com>
2022-04-13 14:14:25 -0500

    router, logs with colors

--- server.ts
+++ server.ts
@@ -1,15 +1,49 @@
-import { Application } from "https://deno.land/x/oak/mod.ts";
-		
-const app = new Application();
+import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { green, yellow } from "https://deno.land/std@0.53.0/fmt/colors.ts";
+
 const port: number = 8080;
 
-app.use((ctx) => {
-  ctx.response.body = "Hello World!";
+const router = new Router();
+router.get("/", (context) => {
+  context.response.body = {
+    message: "Hello World",
+  };
 });
 
+const app = new Application();
 
-console.log('running on port ', port);
+app.use(router.routes());
+app.use(router.allowedMethods());
+
+// Logger
+app.use(async (ctx, next) => {
+  await next();
+  const rt = ctx.response.headers.get("X-Response-Time");
+  console.log(`${ctx.request.method} ${ctx.request.url} - ${rt}`);
+});
+
+// Timing
+app.use(async (ctx, next) => {
+  const start = Date.now();
+  await next();
+  const ms = Date.now() - start;
+  ctx.response.headers.set("X-Response-Time", `${ms}ms`);
+});
+
+// app.use((ctx) => {
+//   ctx.response.body = "Hello World!";
+// });
+
+//Opening the server
+// console.log('Running on port ', port);
+app.addEventListener("listen", ({ hostname, port, secure }) => {
+  const protocol = secure ? "https://" : "http://";
+  const url = `${protocol}${hostname ?? "localhost"}:${port}`;
+  console.log(
+    `${yellow("Listening on:")} ${green(url)}`,
+  );
+});
+
 await app.listen({ port });
-
 
 //deno run --allow-net server.ts
